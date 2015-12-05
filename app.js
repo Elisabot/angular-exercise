9f37e2eaee0a8fee84551a16846fdafa2ac1d2cf
elisabot <elisabeth.nimtzmeyer@gmail.com>
2015-12-04 17:27:09 -0800

    cards now display on html

--- app.js
+++ app.js
@@ -4,123 +4,9 @@ console.log("we're connected");
 
 	var app = angular.module('cardApp', [])
 
-	app.controller('mainCtrl', function ($scope) {
-		$scope.shuffleButton = function () {
-			console.log("this is shuffleButton");
-		};
-		$scope.sortButton = function () {
-			console.log("this is sortButton")
-		};
-
 	app.controller('cardCtrl', ['$scope', function($scope) {
 		$scope.cards = 
 		[
-			// Club Suit
-			{
-				name:"club-a",
-				img: 'img/deck/club-A.png'
-			},
-			{
-				name:"club-2",
-				img: 'img/deck/club-2.png'
-			},
-			{
-				name:"club-3",
-				img: 'img/deck/club-3.png'
-			},
-			{
-				name:"club-4",
-				img: 'img/deck/club-4.png'
-			},
-			{
-				name:"club-5",
-				img: 'img/deck/club-5.png'
-			},
-			{
-				name:"club-6",
-				img: 'img/deck/club-6.png'
-			},
-			{
-				name:"club-7",
-				img: 'img/deck/club-7.png'
-			},
-			{
-				name:"club-8",
-				img: 'img/deck/club-8.png'
-			},
-			{
-				name:"club-9",
-				img: 'img/deck/club-9.png'
-			},
-			{
-				name:"club-10",
-				img: 'img/deck/club-10.png'
-			},
-			{
-				name:"club-j",
-				img: 'img/deck/club-J.png'
-			},
-			{
-				name:"club-q",
-				img: 'img/deck/club-Q.png'
-			},
-			{
-				name:"club-k",
-				img: 'img/deck/club-K.png'
-			},
-			//Heart Suit
-			{
-				name:"heart-a",
-				img: 'img/deck/heart-A.png'
-			},
-			{
-				name:"heart-2",
-				img: 'img/deck/heart-2.png'
-			},
-			{
-				name:"heart-3",
-				img: 'img/deck/heart-3.png'
-			},
-			{
-				name:"heart-4",
-				img: 'img/deck/heart-4.png'
-			},
-			{
-				name:"heart-5",
-				img: 'img/deck/heart-5.png'
-			},
-			{
-				name:"heart-6",
-				img: 'img/deck/heart-6.png'
-			},
-			{
-				name:"heart-7",
-				img: 'img/deck/heart-7.png'
-			},
-			{
-				name:"heart-8",
-				img: 'img/deck/heart-8.png'
-			},
-			{
-				name:"heart-9",
-				img: 'img/deck/heart-9.png'
-			},
-			{
-				name:"heart-10",
-				img: 'img/deck/heart-10.png'
-			},
-			{
-				name:"heart-j",
-				img: 'img/deck/heart-J.png'
-			},
-			{
-				name:"heart-q",
-				img: 'img/deck/heart-Q.png'
-			},
-			{
-				name:"heart-k",
-				img: 'img/deck/heart-K.png'
-			},
 			//Spade Suit
 			{
 				name:"spade-a",
@@ -227,10 +113,122 @@ console.log("we're connected");
 				name:"diamond-k",
 				img: 'img/deck/diamond-K.png'
 			},
+			// Club Suit
+			{
+				name:"club-a",
+				img: 'img/deck/club-A.png'
+			},
+			{
+				name:"club-2",
+				img: 'img/deck/club-2.png'
+			},
+			{
+				name:"club-3",
+				img: 'img/deck/club-3.png'
+			},
+			{
+				name:"club-4",
+				img: 'img/deck/club-4.png'
+			},
+			{
+				name:"club-5",
+				img: 'img/deck/club-5.png'
+			},
+			{
+				name:"club-6",
+				img: 'img/deck/club-6.png'
+			},
+			{
+				name:"club-7",
+				img: 'img/deck/club-7.png'
+			},
+			{
+				name:"club-8",
+				img: 'img/deck/club-8.png'
+			},
+			{
+				name:"club-9",
+				img: 'img/deck/club-9.png'
+			},
+			{
+				name:"club-10",
+				img: 'img/deck/club-10.png'
+			},
+			{
+				name:"club-j",
+				img: 'img/deck/club-J.png'
+			},
+			{
+				name:"club-q",
+				img: 'img/deck/club-Q.png'
+			},
+			{
+				name:"club-k",
+				img: 'img/deck/club-K.png'
+			},
+			//Heart Suit
+			{
+				name:"heart-a",
+				img: 'img/deck/heart-A.png'
+			},
+			{
+				name:"heart-2",
+				img: 'img/deck/heart-2.png'
+			},
+			{
+				name:"heart-3",
+				img: 'img/deck/heart-3.png'
+			},
+			{
+				name:"heart-4",
+				img: 'img/deck/heart-4.png'
+			},
+			{
+				name:"heart-5",
+				img: 'img/deck/heart-5.png'
+			},
+			{
+				name:"heart-6",
+				img: 'img/deck/heart-6.png'
+			},
+			{
+				name:"heart-7",
+				img: 'img/deck/heart-7.png'
+			},
+			{
+				name:"heart-8",
+				img: 'img/deck/heart-8.png'
+			},
+			{
+				name:"heart-9",
+				img: 'img/deck/heart-9.png'
+			},
+			{
+				name:"heart-10",
+				img: 'img/deck/heart-10.png'
+			},
+			{
+				name:"heart-j",
+				img: 'img/deck/heart-J.png'
+			},
+			{
+				name:"heart-q",
+				img: 'img/deck/heart-Q.png'
+			},
+			{
+				name:"heart-k",
+				img: 'img/deck/heart-K.png'
+			}
 		]
-	}
-		
-	])
 
-	});
+		$scope.shuffleButton = function () {
+			console.log("this is shuffleButton");
+		}
+
+		$scope.sortButton = function () {
+			console.log("this is sortButton");
+		}
+
+	}])
+
 })();
